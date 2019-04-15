4ad37cfabf551db130421a39785575bbd8a73bcc
TaghreedReda <taghreedzredaz@gmail.com>
2019-04-15 19:34:01 +0200

    bootstrap-stuff

---
+++ sol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector <int> t;
+        for (int i=0 ;i<nums.size();i++){
+            for (int j=i+1 ;j<nums.length();j++){
+                if (nums[i]+nums[j]==target)
+                    t.pushback(i), t.push_back(j);
+            }
+        }
+        return t;
+    }
+};
